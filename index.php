7ce29d2bb8085a92eccc29b60d88ce9ebddf7af8
Miggy <mhigseptian@gmail.com>
2019-10-12 05:11:01 +0700

    pesan

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<?php
+	echo "<center>";
+	echo "<b>";
+	echo "10116192";
+	echo "<br>";
+	echo "Miggy Virgiawan Restiana Septian";
+	?>
+
+</body>
+</html>
